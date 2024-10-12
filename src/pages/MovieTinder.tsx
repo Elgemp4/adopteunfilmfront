2fe69aacb7a669188a121ffdff47f7c0675d5648
Emilien Marquegnies <marquegniesemilien@gmail.com>
2024-10-12 10:37:52 +0200

    Fixed style issues added icons, and organize a bit more the sass

--- src/pages/MovieTinder.tsx
+++ src/pages/MovieTinder.tsx
@@ -3,8 +3,8 @@ import { useState } from 'react';
 export default function MovieTinder() {
     const [movie, setMovie] = useState({
         title: 'Example Movie',
-        description: 'This is an example movie description. Im trying to make it as long as possible to see how it looks when it is expanded.',
-        imageUrl: 'https://via.placeholder.com/300x450'
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel excepturi officiis nam esse explicabo. Quaerat ut consequuntur quo sapiente ex perferendis id consectetur doloremque cumque. Consequatur facilis aperiam iste et.",
+        imageUrl: 'https://placehold.co/300x450'
     });
 
     const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
@@ -28,21 +28,23 @@ export default function MovieTinder() {
     return (
         <div className="movie-tinder">
             <div className="header">
-                <button className="settings-button">Settings</button>
-                <button className="groups-button">Groups</button>
+                <button className="settings-button material-symbols-outlined">Settings</button>
+                <button className="groups-button material-symbols-outlined">Groups</button>
             </div>
             <div className="movie-container">
                 <img src={movie.imageUrl} alt={movie.title} className="movie-image" />
                 <div className="preferencesButtons-container">
-                    <button className="dislike-button" onClick={handleDislike}>Je n'aime pas</button>
-                    <button className="seen-button" onClick={handleSeen}>Déjà vu</button>
-                    <button className="like-button" onClick={handleLike}>J'aime</button>
+                    <button className="dislike-button material-symbols-outlined" onClick={handleDislike}>thumb_down</button>
+                    <button className="seen-button material-symbols-outlined" onClick={handleSeen}>visibility</button>
+                    <button className="like-button material-symbols-outlined" onClick={handleLike}>thumb_up</button>
                 </div>
                 <div className={`movie-description ${isDescriptionExpanded ? 'expanded' : ''}`}>
                     <button className="expand-button" onClick={toggleDescription}>
                         {isDescriptionExpanded ? 'Réduire' : 'Agrandir'}
                     </button>
-                    {movie.description}
+                    <div>
+                        {movie.description}
+                    </div>
                 </div>
             </div>
         </div>
