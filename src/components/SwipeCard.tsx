dd02bd36843182a15041a85325934ae985d070ec
Emilien Marquegnies <marquegniesemilien@gmail.com>
2024-10-12 12:53:18 +0200

    Tinder swipe working

--- src/components/SwipeCard.tsx
+++ src/components/SwipeCard.tsx
@@ -1,13 +1,43 @@
 import { useMovieContext } from "../contexts/MovieContext";
 
+import { motion, useMotionValue, useTransform } from "framer-motion"
+
 export default function SwipeCard() {
     const movieContext = useMovieContext();
+
+    const x = useMotionValue(0);
+
+    const opacity = useTransform(x, [-100, 0, 100], [0.1, 1, 0.1]);
+    const rotate = useTransform(x, [-100, 0, 100], [-10, 0, 10]);
 
     if(movieContext == undefined){
         throw new Error("Context undefined");
     }
 
-    const {movie_image, movie_title} = movieContext;
+    const {movie_image, movie_title, like, dislike} = movieContext;
 
-    return <img src={movie_image} alt={movie_title} className="movie-image" />   
+    const handleDragEnd = () => {
+        if(x.get() > 100){
+            like()
+        }
+        else if(x.get() < -100){
+            dislike()
+        }
+    }
+
+    return <motion.img 
+    src={movie_image} 
+    alt={movie_title}
+    style={{
+        x,
+        opacity,
+        rotate
+    }}
+    className="movie-image" 
+    drag="x"
+    dragConstraints={{
+        left: 0,
+        right: 0,
+    }}
+    onDragEnd={handleDragEnd}/>   
 }
