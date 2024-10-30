578789a05d0166e444d635b95d3f536e75a2976e
Elgemp4 <69595917+Elgemp4@users.noreply.github.com>
2024-10-30 13:56:06 +0100

    Added page style

--- src/pages/UserSettings.tsx
+++ src/pages/UserSettings.tsx
@@ -1,8 +1,45 @@
+import Button from "../components/forms/Button";
+import Input from "../components/forms/Input"
+import { useAuth } from "../contexts/AuthContext";
+
 export default function UserSettings() {
-return (
-    <div>
-        <h1>Settings</h1>
-        <p>Here you can see the settings</p>
-    </div>
-);
+    const { birthDate, firstname, lastname, setBirthDate, setFirstname, setLastname } = useAuth();
+
+    return <form className="form-container">
+            <Input
+                name="lastname"
+                title="Nom  : "
+                key="lastname"
+                value={lastname}
+                onValueChange={setLastname}
+            />
+            <Input
+                name="firstname"
+                title="Prénom  : "
+                key="firstname"
+                value={firstname}
+                onValueChange={setFirstname}
+            />
+            <Input
+                name="birthdate"
+                title="Date de naissance : "
+                key="birthdate"
+                type="date"
+                value={birthDate}
+                onValueChange={setBirthDate}
+            />
+            <div className="button-container button-container__vertical">
+                <Button
+                    name="provider"
+                    text="Modifier fournisseurs"
+                    key="provider"
+                    type="button"/>
+                <Button
+                    name="submit"
+                    text="Enregistrer"
+                    key="save"
+                type="submit"/>
+            </div>
+        </form>
+
 }
