e83ba4f4246d0fd464fab0983912891a44f28073
DrKreijger <sebdek7@gmail.com>
2024-11-14 16:04:25 +0100

    Changed movie layout

--- src/components/MovieDescription.tsx
+++ src/components/MovieDescription.tsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import { useMovieContext } from "../contexts/MovieContext";
 
 export default function MovieDescription() {
-    const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
-
     const movieContext = useMovieContext();
 
     if(movieContext == undefined){
@@ -12,14 +9,7 @@ export default function MovieDescription() {
 
     const {movie_description, movie_title} = movieContext;
 
-    const toggleDescription = () => {
-        setIsDescriptionExpanded(!isDescriptionExpanded);
-    };
-
-    return <div className={`movie-info ${isDescriptionExpanded ? 'expanded' : ''}`}>
-        <button className="expand-button material-symbols-outlined" onClick={toggleDescription}>
-            {isDescriptionExpanded ? 'collapse_content' : 'expand_content'}
-        </button>
+    return <div className={`movie-info`}>
         <div>
             <h3 className="movie-info__title">{movie_title}</h3>
             <p className="movie-info__description">{movie_description}</p>
