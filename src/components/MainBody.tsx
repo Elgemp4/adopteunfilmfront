768592582619270b8c797b37ef386ec2a4ec2709
DrKreijger <sebdek7@gmail.com>
2024-12-12 13:51:20 +0100

    Fixed top bar on login
    Fixed top bar showing on login page

--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import TopBar from '../components/TopBar';
 import '../style/components/_main_body.sass';
+import Private from '../routers/Private';
 
 export default function MainBody() {
     return (
         <>
-            <TopBar />
+            <Private>
+                <TopBar/>
+            </Private>
             <div className="main-body">
-                <Outlet />
+                <Outlet/>
             </div>
         </>
     );
